68427dd18cf570bdba25c6eccca06ad6f76f7418
shenlian <1019947054@qq.com>
2014-05-25 21:56:22 +0800

    finish registration

--- registration/models.py
+++ registration/models.py
@@ -13,8 +13,10 @@ from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
 from django.contrib.sites.models import get_current_site,Site
 from django.db import models
-#from backend.logging import logger,loginfo
+from backend.logging import logger,loginfo
 
+from const import *
+from const.models import UserIdentity
 SHA1_RE = re.compile('^[a-f0-9]{40}$')      #Activation Key
 
 class RegistrationManager(models.Manager):
@@ -47,8 +49,8 @@ class RegistrationManager(models.Manager):
         return False
     @transaction.commit_on_success
     def create_inactive_user(self,request,
-                             username,password,email
-                            ,send_email=True, profile_callback=None, **kwargs):
+                             username,password,email,
+                             send_email=True, profile_callback=None, **kwargs):
         """
         Create a new, inactive ``User``, generates a
         ``RegistrationProfile`` and email its activation key to the
@@ -58,100 +60,39 @@ class RegistrationManager(models.Manager):
 
         """
         #如果存在用户的话不必进行新建只需对权限表进行操作即可，否则新建用户
-#        loginfo("person_name:" + kwargs["person_name"])
-#        if User.objects.filter(email=email).count() == 0:
-        new_user = User.objects.create_user(username, email, password)
-#            new_user.is_active = False
-#            new_user.save()
-#            registration_profile = self.create_profile(new_user)
-#            registration_profile.save()
-#            current_site = Site.objects.get_current()
-#            site_domain=current_site.domain
-#            if send_email:
-#                from django.core.mail import send_mail
-#                subject = render_to_string('registration/activation_email_subject.txt',
-#                                           {'site':get_current_site(request),
-#                                            'school_name':SCHOOL_NAME,
-#                                            'username':username,
-#                                            'password':password})
-
-#                 Email subject *must not* contain newlines
-#                subject = ''.join(subject.splitlines())
-#                message = render_to_string('registration/activation_email.txt',
-#                                           {'activation_key':registration_profile.activation_key,
-#                                            'expiration_days':settings.ACCOUNT_ACTIVATION_DAYS,
-#                                            'school_name':SCHOOL_NAME,
-#                                            'year':get_current_year(),
-#                                            'site':site_domain,
-#                                            'username':username,
-#                                            'password':password}
-#                                           )
-#                logger.error(message)
-#                send_mail(subject,
-#                          message,
-#                          settings.DEFAULT_FROM_EMAIL,
-#                          [new_user.email])
-#        else:
-#            new_user = User.objects.get(email=email)
+        if User.objects.filter(email=email).count() == 0:
+            new_user = User.objects.create_user(username, email, password)
+            new_user.is_active = False
+            new_user.save()
+            registration_profile = self.create_profile(new_user)
+            registration_profile.save()
+            current_site = Site.objects.get_current()
+            site_domain=current_site.domain
+            if send_email:
+                from django.core.mail import send_mail
+                subject = render_to_string('registration/activation_email_subject.txt',
+							              {'site':get_current_site(request),
+								           'username':username,
+								           'password':password})
+                subject = ''.join(subject.splitlines())
+                message = render_to_string('registration/activation_email.txt',
+						                  {'activation_key':registration_profile.activation_key,
+							               'expiration_days':settings.ACCOUNT_ACTIVATION_DAYS,
+							               'site':site_domain,
+							               'username':username,
+							               'password':password})
+                logger.error(message)
+                send_mail(subject,message,settings.DEFAULT_FROM_EMAIL,[new_user.email])
+        else:
+			new_user = User.objects.get(email=email)
 
 #        对用户权限写入数据库
-#        new_authority = UserIdentity.objects.get(identity=Identity)
-#        new_authority.auth_groups.add(new_user)
-#        new_authority.save()
+        new_authority = UserIdentity.objects.get(identity=NORMAL_USER)
+        new_authority.auth_groups.add(new_user)
+        new_authority.save()
 
-#        如果是学校注册 添加学校注册姓名
-#        if kwargs.get('school_name',False):
-#            schoolObj = SchoolDict.objects.get(id = kwargs["school_name"])
-#            if SchoolProfile.objects.filter(school=schoolObj).count() == 0:
-#                loginfo("person_name:"+kwargs["person_name"])
-#                schoolProfileObj = SchoolProfile(school=schoolObj, userid =new_user,name = kwargs["person_name"])
-#                schoolProfileObj.save()
-#                project_is_assigned = Project_Is_Assigned(school=schoolProfileObj)
-#                project_is_assigned.save()
-#            else:
-#                schoolProfileObj = SchoolProfile.objects.get(school=schoolObj)
-
-#                oldUserObj = schoolProfileObj.userid
-#                for obj in ProjectFinishControl.objects.filter(userid = oldUserObj):
-#                    obj.userid = new_user
-#                    obj.save()
-#                覆盖某些对象的外键关系
-
-#                schoolProfileObj.userid = new_user
-#                schoolProfileObj.name  = kwargs["person_name"]
-#                schoolProfileObj.save()
-#                if ExpertProfile.objects.filter(userid = oldUserObj).count() == 0 \
-#                    and TeacherProfile.objects.filter(userid = oldUserObj).count() == 0:
-#                    oldUserObj.delete() #删除被覆盖用户
-
-#        elif kwargs.get('teacher_school', False):
-#            teacherProfileObj = TeacherProfile(school=kwargs["teacher_school"], userid =new_user,name = kwargs["person_name"])
-#            teacherProfileObj.save()
-#            teacherProjLimit = TeacherProjectPerLimits(teacher=teacherProfileObj,
-#                                                       number=0)
-#            teacherProjLimit.save()
-
-#        elif kwargs.get("expert_user", False):
-#             insituteObj = InsituteCategory.objects.get(id=kwargs["expert_insitute"])
-#            expertProfileObj = ExpertProfile(userid =new_user,name = kwargs["person_name"])
-#            if kwargs["expert_user"] == "assigned_by_school":
-#                expertProfileObj.grade = "0"
-#                expertProfileObj.assigned_by_school = SchoolProfile.objects.get(userid = request.user)
-#            else:
-#                expertProfileObj.grade = "1"
-#                expertProfileObj.assigned_by_adminstaff = AdminStaffProfile.objects.get(userid = request.user)
-#            expertProfileObj.save()
-
-#        elif kwargs.get("student_user",False):
-#            teacher_name = request.user.username
-#            teacher = User.objects.get(username=teacher_name)
-#            teacher_profile = TeacherProfile.objects.get(userid = teacher)
-#            student_obj = StudentProfile(userid = new_user,teacher = teacher_profile,name = kwargs["person_name"])
-#            student_obj.save()
-#        else:
-#            raise Http404
-#        if profile_callback is not None:
-#            profile_callback(user=new_user)
+        if profile_callback is not None:
+            profile_callback(user=new_user)
         return new_user
 
     def create_profile(self,user):
